8b0971a4ca6dfb8ec12bc35edd92ad22c0fc3849
Tanya <36923363+tantarin@users.noreply.github.com>
2026-03-24 16:39:44 +0200

    Update sw2: Longest Repeating Char Replacement

--- problems.js
+++ problems.js
@@ -2215,16 +2215,14 @@ code:`class Solution {
 
         for (int right = 0; right < s.length(); right++) {
             freq[s.charAt(right) - 'A']++;
-            maxFreq = Math.max(maxFreq,
-                freq[s.charAt(right) - 'A']);
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
 
             while (right - left + 1 - maxFreq > k) {
                 freq[s.charAt(left) - 'A']--;
                 left++;
             }
 
-            result = Math.max(result,
-                right - left + 1);
+            result = Math.max(result, right - left + 1);
         }
 
         return result;

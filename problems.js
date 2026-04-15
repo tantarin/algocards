1fffb59910dad20ba736b059994bb0d31b7fc2a9
Tanya <36923363+tantarin@users.noreply.github.com>
2026-04-15 12:24:54 +0200

    Update sp1: LC 14 Longest Common Prefix

--- problems.js
+++ problems.js
@@ -3756,8 +3756,7 @@ code:`class Solution {
             char c = strs[0].charAt(i);
 
             for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length()
-                    || strs[j].charAt(i) != c) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
                     return strs[0].substring(0, i);
                 }
             }

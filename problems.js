031213d370dad68d1cbd1f82186a53d650865885
Tanya <36923363+tantarin@users.noreply.github.com>
2026-04-15 17:52:53 +0200

    Update tp20: Рост акций компании

--- problems.js
+++ problems.js
@@ -4578,13 +4578,11 @@ code:`class Solution {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-
         for (int price : prices) {
             if (price < minPrice) {
                 minPrice = price;
             } else {
-                maxProfit = Math.max(maxProfit,
-                    price - minPrice);
+                maxProfit = Math.max(maxProfit, price - minPrice);
             }
         }
 

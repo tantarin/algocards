e081761293a128354e2ea6d3fa57d38649afc462
Tanya <36923363+tantarin@users.noreply.github.com>
2026-04-20 21:08:16 +0200

    Update ya12: LC 721 · Accounts Merge

--- problems.js
+++ problems.js
@@ -8677,18 +8677,28 @@ code:`class Solution {
         DSU(int n) {
             parent = new int[n];
             rank = new int[n];
-            for (int i = 0; i < n; i++) parent[i] = i;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
         }
+
         int find(int x) {
-            if (parent[x] != x) parent[x] = find(parent[x]);
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
             return parent[x];
         }
+
         void union(int a, int b) {
             int ra = find(a), rb = find(b);
             if (ra == rb) return;
-            if (rank[ra] < rank[rb]) parent[ra] = rb;
-            else if (rank[ra] > rank[rb]) parent[rb] = ra;
-            else { parent[rb] = ra; rank[ra]++; }
+            if (rank[ra] < rank[rb]) {
+                parent[ra] = rb;
+            } else if (rank[ra] > rank[rb]) {
+                parent[rb] = ra;
+            } else { 
+                parent[rb] = ra; rank[ra]++; 
+            }
         }
     }
 

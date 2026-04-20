390717e6b803b7b1289933bddec01a1740f595cb
Tanya <36923363+tantarin@users.noreply.github.com>
2026-04-20 19:00:57 +0200

    Update ll2: Удалите N-й узел с конца

--- problems.js
+++ problems.js
@@ -1713,9 +1713,9 @@ code:`class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+
         ListNode fast = dummy;
         ListNode slow = dummy;
-
         for (int i = 0; i <= n; i++) {
             fast = fast.next;
         }

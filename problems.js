56c5437b2b45885c9f590872e830b534960214b7
Tanya <36923363+tantarin@users.noreply.github.com>
2026-04-27 13:13:35 +0300

    Update hf8: Count Pairs With Absolute Diff K

--- problems.js
+++ problems.js
@@ -6037,11 +6037,11 @@ code:`class Solution {
     public int countKDifference(int[] nums, int k) {
         Map<Integer, Integer> freq = new HashMap<>();
         int count = 0;
-
         for (int num : nums) {
             count += freq.getOrDefault(num - k, 0);
             count += freq.getOrDefault(num + k, 0);
-            freq.merge(num, 1, Integer::sum);
+
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
         return count;

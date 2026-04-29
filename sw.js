1c6b1f39f040b924f60c431a4f0bb815aaae4f15
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2026-04-29 19:16:55 +0300

    Strengthen selectable card titles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'algocards-v34';
+const CACHE = 'algocards-v35';
 /** problems.js и theory.js часто меняются через API — не включаем в precache (устаревает мгновенно) */
 const ASSETS = ['./index.html', './manifest.json'];
 

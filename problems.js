392c6a9b42bfef816f09fe8d8813592e107a62bc
Tanya <36923363+tantarin@users.noreply.github.com>
2026-05-01 22:35:58 +0300

    Update lc2: LC 2 · Add Two Numbers

--- problems.js
+++ problems.js
@@ -9268,10 +9268,12 @@ code:`class Solution {
             int sum = a + b + carry;
             carry = sum / 10;
             cur.next = new ListNode(sum % 10);
+
             cur = cur.next;
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
         }
+
         return dummy.next;
     }
 }`,

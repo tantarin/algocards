77eca6e0528fd153029123e0291dca0f2a1d788a
Tanya <36923363+tantarin@users.noreply.github.com>
2026-05-02 14:38:10 +0300

    Update hf2: Слово-анаграмма

--- problems.js
+++ problems.js
@@ -1328,14 +1328,15 @@ code:`class Solution {
         if (s.length() != t.length()) return false;
 
         int[] count = new int[26];
-
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
 
         for (int c : count) {
-            if (c != 0) return false;
+            if (c != 0) {
+               return false;
+            }
         }
 
         return true;
